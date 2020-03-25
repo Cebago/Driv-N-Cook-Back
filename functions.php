3faf0c84f1af66e79eab7437e5ecff05c9b69522
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-25 18:44:05 +0100

    Create a function to connect DB

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
 <?php
+require_once "conf.inc.php";
+
+function connectDB(){
+    try{
+        $pdo = new PDO( DBDRIVER.":host=".DBHOST.";dbname=".DBNAME ,DBUSER,DBPWD);
+        //Permet d'afficher les erreurs SQL
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(Exception $e){
+        die("Erreur SQL : ".$e->getMessage());
+    }
+    return $pdo;
+}
