7a254c772424c94017e3a05f6356bf5969b294d2
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-25 19:08:24 +0100

    Update pwd for database

--- conf.inc.php
+++ conf.inc.php
@@ -3,4 +3,4 @@ define("DBNAME", "pa2a2drivncook");
 define("DBHOST", "51.178.31.223:13798");
 define("DBDRIVER", "mysql");
 define("DBUSER", "cebago");
-define("DBPWD", "@ESGI-cebago");
+define("DBPWD", "ESGI-cebago");
