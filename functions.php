74c43d82c90786a5bbc527e03a7d64fc63387047
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-26 09:04:50 +0100

    Update functions

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ require_once "conf.inc.php";
 
 function connectDB(){
     try{
-        $pdo = new PDO( DBDRIVER.":host=".DBHOST.";dbname=".DBNAME ,DBUSER,DBPWD);
+        $pdo = new PDO(DBDRIVER.":host=".DBHOST.";dbname=".DBNAME ,DBUSER,DBPWD);
         //Permet d'afficher les erreurs SQL
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(Exception $e){
