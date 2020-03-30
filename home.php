dc38b67c48b7b9a54c6e4f05710a19f52b8df2b1
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-30 13:32:39 +0200

    Create content in home

--- home.php
+++ home.php
@@ -4,8 +4,35 @@ include 'header.php';
 require 'conf.inc.php';
 require 'functions.php'; ?>
 </head>
+
 <body>
     <?php include 'navbar.php'; ?>
+    <div class="card-deck w-75 mx-auto mt-5">
+        <div class="card menu">
+            <img src="https://www.humanprogresscenter.com/wp-content/uploads/2016/05/fond-gris.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Gestion de l'entreprise</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card menu">
+            <img src="https://www.humanprogresscenter.com/wp-content/uploads/2016/05/fond-gris.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card menu">
+            <img src="https://www.humanprogresscenter.com/wp-content/uploads/2016/05/fond-gris.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
     <?php include 'footer.php'; ?>
 </body>
 
