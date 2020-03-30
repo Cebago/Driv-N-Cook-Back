1e96ea3b5474269b89683749d28dd65a377e6ae4
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-30 10:19:29 +0200

    change value of header title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <head>
     <meta lang="FR">
     <meta charset="UTF-8">
-    <meta title="Espace administration">
+    <title>Espace administration</title>
     <meta name="Description" content="Site d'administration de Driv'n Cook">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     
