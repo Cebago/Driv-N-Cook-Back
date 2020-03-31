a95a0603eb4e9bb3e6415bca63a5b1f4618cecc7
Baptiste DEMARCHE <batmine3@gmail.com>
2020-03-31 18:40:56 +0200

    Those files aren't in the register release

--- franchisees.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include 'header.php';
-require "conf.inc.php";
-require "functions.php"; ?>
-</head>
-<body>
-    Gestion des franchisés
-</body>
--- salesHistory.php
+++
@@ -1,1 +0,0 @@
-salesHistory
