fec699233ef429f5b131abcecf91e9d232f0c48d
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-04 12:39:09 +0200

    Create a javascript function with the pie Chart

--- sales.php
+++ sales.php
@@ -1,1 +1,37 @@
-List des revenus
+<?php
+session_start();
+include 'header.php';
+require 'conf.inc.php';
+require 'functions.php'; ?>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <script>
+        function jsSales() {
+            let chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                exportEnabled: true,
+                title:{
+                    text: "Répartition du chiffre d'affaires par franchisé"
+                },
+                subtitles: [{
+                    text: "Devise utilisée: Euro (€)"
+                }],
+                data: [{
+                    type: "pie",
+                    showInLegend: "true",
+                    legendText: "{label}",
+                    indexLabelFontSize: 16,
+                    indexLabel: "{label} - #percent%",
+                    yValueFormatString: "#.##€",
+                    dataPoints: <?php echo phpSales(); ?>
+                }]
+            });
+            chart.render();
+        }
+        window.onload = jsSales;
+        setInterval(jsSales, 300000);
+    </script>
+    <div id="chartContainer" class="card w-75 mx-auto"></div>
+    <?php include 'footer.php'; ?>
+</body>
