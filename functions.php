230fd092e4d6d98a184088d0f0df1d6c5c92f674
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-04 12:40:14 +0200

    Function to get php data and return it at json string

--- functions.php
+++ functions.php
@@ -29,3 +29,21 @@ function login($email){
     $_SESSION["token"] = $token;
     $_SESSION["email"] = $email;
 }
+
+function phpSales():string {
+    $pdo = connectDB();
+    $queryPrepared = $pdo->prepare("SELECT truckmanufacturers, SUM(orderPrice) as sum FROM ORDERS, TRUCK WHERE ORDERS.truck = TRUCK.idTruck
+    AND orderDate BETWEEN :dateBegin AND :dateEnd GROUP BY idTruck;");
+    $queryPrepared->execute(
+        [
+            ":dateBegin" => getdate(time())["year"]."-01-01",
+            ":dateEnd" => getdate(time())["year"]."-12-31"
+        ]
+    );
+    $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+    $dataPoints = array();
+    foreach ($result as $truck) {
+        $dataPoints[] = array("label" => $truck["truckmanufacturers"], "y" => $truck["sum"]);
+    }
+    return json_encode($dataPoints, JSON_NUMERIC_CHECK);
+}
