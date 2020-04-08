f14c89689170dd0c42fa49f2e3c3235b88907560
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-09 00:42:00 +0200

    delete debug messages

--- functions/unassignDriver.php
+++ functions/unassignDriver.php
@@ -2,7 +2,7 @@
     session_start();
     require "../conf.inc.php";
     require "../functions.php";
-
+    
     if (isset($_POST["truck"])) {
         $truck = $_POST["truck"];
         $error = false;
@@ -14,12 +14,8 @@
         }
         
         $pdo = connectDB();
-        $queryPrepared = $pdo->prepare("SELECT user FROM TRUCK WHERE idTruck = :truck");
+        $queryPrepared = $pdo->prepare("UPDATE TRUCK SET user = null WHERE idTruck = :truck");
         $queryPrepared->execute([
-            ":truck" => $truck;
+            ":truck" => $truck
         ]);
-        $result = $queryPrepared->fetchAll();
-        echo "<pre>" . print_r($result) . "</pre>";
-        
-        
     }
