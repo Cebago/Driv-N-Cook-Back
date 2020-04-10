b6b68ae9f88757f0df7084d7ee0c24156cbab372
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-10 16:03:24 +0200

    alert when user is already assigned

--- trucks.php
+++ trucks.php
@@ -92,7 +92,9 @@ require 'functions.php';
         request.onreadystatechange = function() {
             if(request.readyState === 4) {
                 if(request.status === 200) {
-                    console.log(request.responseText);
+                    if (request.responseText !== "") {
+                        alert(request.responseText);
+                    }
                 }
             }
         };
