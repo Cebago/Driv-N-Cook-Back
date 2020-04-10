916cbfdaacd4d60043415703355934a787c4149b
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-10 09:50:21 +0200

    Ajax method on chart sales

---
+++ functions/getSales.php
@@ -0,0 +1,21 @@
+<?php
+
+    session_start();
+    require "../conf.inc.php";
+    require "../functions.php";
+
+    $pdo = connectDB();
+    $queryPrepared = $pdo->prepare("SELECT truckmanufacturers, SUM(orderPrice) as sum FROM ORDERS, TRUCK WHERE ORDERS.truck = TRUCK.idTruck
+    AND orderDate BETWEEN :dateBegin AND :dateEnd GROUP BY idTruck;");
+    $queryPrepared->execute(
+        [
+            ":dateBegin" => getdate(time())["year"]."-01-01",
+            ":dateEnd" => getdate(time())["year"]."-12-31"
+        ]
+    );
+    $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+    $dataPoints = array();
+    foreach ($result as $truck) {
+        $dataPoints[] = array("label" => $truck["truckmanufacturers"], "y" => $truck["sum"]);
+    }
+    echo "".json_encode($dataPoints, JSON_NUMERIC_CHECK);
