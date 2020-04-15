907f4600138043314db5e1d0f4a1bcb2fc3db696
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-15 19:53:31 +0200

    Modal to create a new truck

--- trucks.php
+++ trucks.php
@@ -9,6 +9,10 @@ require 'functions.php';
 <body>
 <?php include "navbar.php"; ?>
 <div class="menu mt-5 card col-md-11 mx-auto">
+    <h5 class="card-header">Gestion de l'ensemble des camions</h5>
+    <div class="card-body">
+        <button type="button" class="btn btn-primary ml-5 mr-5 mx-auto" data-toggle="modal" data-target="#createTruckModal" ><i class="fas fa-plus"></i>&nbsp;Ajouter un nouveau camion</button>
+    </div>
     <table class="table table-striped mt-2">
         <thead class="thead-dark">
         <tr>
@@ -57,60 +61,120 @@ require 'functions.php';
                 </select>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                <button class="btn btn-secondary" data-dismiss="modal">Annuler</button>
                 <button class="btn btn-primary" data-dismiss="modal" type="button" onclick="assignTruck()">Assigner</button>
             </div>
         </div>
     </div>
 </div>
 
-<div class="modal fade" id="updateTruck" tabindex="-1" role="dialog" aria-labelledby="updateTruckInfo" aria-hidden="true">
+<div class="modal fade" id="createTruckModal" tabindex="-1" role="dialog" aria-labelledby="createTruck" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="updateTruckInfo">Modifier le camion</h5>
+                <h5 class="modal-title" id="createTruck">Modifier le camion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="input-group flex-nowrap">
+                <div class="input-group flex-nowrap mt-1">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="idTruck">Camion n°</span>
+                        <span class="input-group-text" id="manufacturers">Marque</span>
                     </div>
-                    <input type="text" id="update" class="form-control truckID" name="idTruck" placeholder="idTruck" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+                    <input type="text" id="truckManufacturers" class="form-control" name="createTruckManufacturers" placeholder="Marque du camion" aria-label="truckId" aria-describedby="addon-wrapping">
                 </div>
                 <div class="input-group flex-nowrap mt-1">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="truckManufacturers">Marque</span>
+                        <span class="input-group-text" id="model">Modèle</span>
                     </div>
-                    <input type="text" id="updateManufacturers" class="form-control truck" name="truckManufacturers" placeholder="Marque du camion" aria-label="truckId" aria-describedby="addon-wrapping">
+                    <input type="text" id="truckModel" class="form-control" name="createTruckModel" placeholder="Modèle du camion" aria-label="truckId" aria-describedby="addon-wrapping">
                 </div>
                 <div class="input-group flex-nowrap mt-1">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="truckModel">Modèle</span>
+                        <span class="input-group-text" id="plate">Plaque d'immatriculation</span>
                     </div>
-                    <input type="text" id="updateModel" class="form-control truck" name="truckModel" placeholder="Modèle du camion" aria-label="truckId" aria-describedby="addon-wrapping">
-                </div>
-                <div class="input-group flex-nowrap mt-1">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="licensePlate">Plaque d'immatriculation</span>
-                    </div>
-                    <input type="text" id="updateLicense" class="form-control truck" name="licensePlate" placeholder="Plaque d'immatriculation du camion" aria-label="truckId" aria-describedby="addon-wrapping">
+                    <input type="text" id="licensePlate" class="form-control" name="createLicensePlate" placeholder="Plaque d'immatriculation du camion" aria-label="truckId" aria-describedby="addon-wrapping">
                 </div>
                 <div class="input-group flex-nowrap mt-1">
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="km">Nombre de kilomètres</span>
                     </div>
-                    <input type="number" id="updateKM" class="form-control truck" name="km" placeholder="Kilomètres parcourus" aria-label="truckId" aria-describedby="addon-wrapping">
+                    <input type="number" id="truckKm" class="form-control" name="createTruckKm" placeholder="Kilomètres parcourus" aria-label="truckId" aria-describedby="addon-wrapping">
+                </div>
+                <div class="input-group mb-3 mt-1">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="truckWarehouse">Options</label>
+                    </div>
+                    <select class="custom-select" id="truckWarehouse">
+                        <option selected>Choisir un entrepôt</option>
+                        <<?php
+                        $pdo = connectDB();
+                        $queryPrepared = $pdo->prepare("SELECT idWarehouse, warehouseName FROM WAREHOUSES WHERE warehouseType = 'Entrepôt'");
+                        $queryPrepared->execute();
+                        $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($result as $value) {
+                            echo "<option value='" . $value["idWarehouse"] . "'>" . $value["warehouseName"] . "</option>";
+                        }
+                        ?>
+                    </select>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                <button class="btn btn-primary" data-dismiss="modal" type="button" onclick="updateTruck()">Modifier</button>
+                <button class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                <button class="btn btn-success" data-dismiss="modal" type="button" onclick="createTruck()">Créer le camion</button>
             </div>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="updateTruck" tabindex="-1" role="dialog" aria-labelledby="updateTruckInfo" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="updateTruckInfo">Modifier le camion</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="input-group flex-nowrap">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="idTruck">Camion n°</span>
+                        </div>
+                        <input type="text" id="update" class="form-control truckID" name="idTruck" placeholder="idTruck" aria-label="truckId" aria-describedby="addon-wrapping" readonly>
+                    </div>
+                    <div class="input-group flex-nowrap mt-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="truckManufacturers">Marque</span>
+                        </div>
+                        <input type="text" id="updateManufacturers" class="form-control truck" name="truckManufacturers" placeholder="Marque du camion" aria-label="truckId" aria-describedby="addon-wrapping">
+                    </div>
+                    <div class="input-group flex-nowrap mt-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="truckModel">Modèle</span>
+                        </div>
+                        <input type="text" id="updateModel" class="form-control truck" name="truckModel" placeholder="Modèle du camion" aria-label="truckId" aria-describedby="addon-wrapping">
+                    </div>
+                    <div class="input-group flex-nowrap mt-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="licensePlate">Plaque d'immatriculation</span>
+                        </div>
+                        <input type="text" id="updateLicense" class="form-control truck" name="licensePlate" placeholder="Plaque d'immatriculation du camion" aria-label="truckId" aria-describedby="addon-wrapping">
+                    </div>
+                    <div class="input-group flex-nowrap mt-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="km">Nombre de kilomètres</span>
+                        </div>
+                        <input type="number" id="updateKM" class="form-control truck" name="km" placeholder="Kilomètres parcourus" aria-label="truckId" aria-describedby="addon-wrapping">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <button class="btn btn-primary" data-dismiss="modal" type="button" onclick="updateTruck()">Modifier</button>
+                </div>
+            </div>
+        </div>
 </div>
 
 <div class="modal fade" id="locateTruck" tabindex="-1" role="dialog" aria-labelledby="locateTruck" aria-hidden="true">
@@ -159,6 +223,7 @@ require 'functions.php';
             truckID[i].value = idTruck;
         }
     }
+    
     function unassignDriver(idtruck) {
         const request = new XMLHttpRequest();
         request.onreadystatechange = function() {
@@ -175,6 +240,7 @@ require 'functions.php';
         );
         refreshTable();
     }
+    
     function assignTruck() {
         const truck = document.getElementById("assign").value;
         const user = document.getElementById("select").value;
@@ -216,6 +282,33 @@ require 'functions.php';
         request.send();
     }
     
+    function createTruck() {
+        const manufacturers = document.getElementById("truckManufacturers").value;
+        const model = document.getElementById("truckModel").value;
+        const license = document.getElementById("licensePlate").value;
+        const km = document.getElementById("truckKm").value;
+        const warehouse = document.getElementById("truckWarehouse").value;
+
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if(request.readyState === 4) {
+                if(request.status === 200) {
+                    console.log(request.responseText);
+                }
+            }
+        };
+        request.open('POST', 'functions/createTruck.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.send(
+            'manufacturers=' + manufacturers +
+            '&model=' + model +
+            '&license=' + license +
+            '&km=' + km +
+            '&warehouse=' + warehouse
+        );
+        refreshTable();
+    }
+    
     function updateTruck() {
         const id = document.getElementById("update")
         const manufacturers = document.getElementById("updateManufacturers");
@@ -244,6 +337,8 @@ require 'functions.php';
         );
         refreshTable();
     }
+    
+    
     setInterval(refreshTable, 60000);
     window.onload = refreshTable;
 </script>
