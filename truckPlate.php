d620899555ae26f241c3fab6306a4eba12c23d0e
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-17 00:02:54 +0200

    Algo to create the license plate

--- truckPlate.php
+++ truckPlate.php
@@ -1,25 +1,25 @@
 <?php
 session_start();
 header("Content-Type: image/png");
-$lenghtCaptcha = rand(6,8);
-$charAuthorized = "abcdefghijklmnopqrstuvwxyz1234567890";
-$charAuthorized = str_shuffle($charAuthorized);
-$captcha = substr($charAuthorized, -$lenghtCaptcha);
+$string = $_GET["string"];
+$lenghtCaptcha = strlen($string);
+$captcha = $string;
 $_SESSION["captcha"] = $captcha;
-$image = imagecreate(300,100);
-$listOfFonts = glob("fonts/*.ttf");
-$startX = rand(10,20);
-$background = imagecolorallocate($image, rand(0,100), rand(0,100), rand(0,100));
-for($i=0; $i<$lenghtCaptcha; $i++){
-    $configCaptcha[$i]["size"] = rand(25,35);
-    $configCaptcha[$i]["font"] = __DIR__."/../".$listOfFonts[array_rand($listOfFonts)];
-    $configCaptcha[$i]["angle"] = rand(-20,20);
+$image = imagecreate(400,100);
+$listOfFonts = "./fonts/dealerplate california.ttf";
+$startX = 60;
+$background = imagecolorallocate($image, 255, 255, 255);
+for ($i=0; $i<$lenghtCaptcha; $i++) {
+    $configCaptcha[$i]["size"] = 35;
+    $configCaptcha[$i]["font"] = __DIR__."/".$listOfFonts;
+    $configCaptcha[$i]["angle"] = 0;
     $configCaptcha[$i]["x"] = (isset($configCaptcha[$i-1]))
-        ?$configCaptcha[$i-1]["x"]+rand(30,40)
+        ?$configCaptcha[$i-1]["x"] + 30
         :$startX;
-    $configCaptcha[$i]["y"] = rand(40,80);
-    $configCaptcha[$i]["color"] = imagecolorallocate($image, rand(150,255), rand(150,255), rand(150,255));
-    imagettftext($image,
+    $configCaptcha[$i]["y"] = 60;
+    $configCaptcha[$i]["color"] = imagecolorallocate($image, 0, 0, 0);
+    imagettftext(
+        $image,
         $configCaptcha[$i]["size"],
         $configCaptcha[$i]["angle"],
         $configCaptcha[$i]["x"],
@@ -29,27 +29,12 @@ for($i=0; $i<$lenghtCaptcha; $i++){
         $captcha[$i]
     );
 }
-$geometryNb = rand(5,10);
-for($j=0;$j<$geometryNb;$j++){
-    $geometry = rand(0,3);
-    switch ($geometry) {
-        case 0:
-            imageline($image, rand(0,300), rand(0,100), rand(0,300), rand(0,100), $configCaptcha[rand(0,$i-1)]["color"]);
-            break;
-        case 1:
-            imageellipse($image, rand(0,300), rand(0,100), rand(0,300), rand(0,100), $configCaptcha[rand(0,$i-1)]["color"]);
-            break;
-
-        case 2:
-            imagerectangle($image, rand(0,300), rand(0,100), rand(0,300), rand(0,100), $configCaptcha[rand(0,$i-1)]["color"]);
-            break;
-
-        case 3:
-            imagepolygon($image, [rand(0,300),rand(0,300),rand(0,300),rand(0,300),rand(0,300),rand(0,300)], 3, $configCaptcha[rand(0,$i-1)]["color"]);
-            break;
-        default:
-            die("Erreur de programmation");
-            break;
-    }
-}
+$left = imagecreate(40, 100);
+$colorLeft = imagecolorallocate($left, 0, 0, 255);
+$right = imagecreate(40, 100);
+$colorRight = imagecolorallocate($right, 0, 0, 255);
+imagerectangle($left, 0, 0, 40, 100, $colorLeft);
+imagerectangle($right, 0, 0, 40, 100, $colorRight);
+imagecopymerge($image, $left, 0, 0, 0, 0, 40, 100, 100 );
+imagecopymerge($image, $right, 360, 0, 0, 0, 40, 100, 100 );
 imagepng($image);
