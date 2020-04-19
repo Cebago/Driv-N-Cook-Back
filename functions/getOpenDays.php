9b628d685fe1d7a19e8788e3426af6b858a7bc62
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-19 21:27:17 +0200

    PHP script to display openDays

--- functions/getOpenDays.php
+++ functions/getOpenDays.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+require "../conf.inc.php";
+require "../functions.php";
+
+$truck = $_POST["truck"];
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("SELECT openDay, DATE_FORMAT(startHour,'%H:%i') as startHour, DATE_FORMAT(endHour,'%H:%i') as endHour  FROM OPENDAYS, TRUCK WHERE truck = idTruck AND truck = :truck");
+$queryPrepared->execute([
+    ":truck" => $truck
+]);
+$result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($result);
+echo $json;
