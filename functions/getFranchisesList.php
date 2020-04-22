8915c4124aa57fcb16d314b4f9325a6c8b0939b2
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-22 22:16:21 +0200

    return a json containing franchises info

--- functions/getFranchisesList.php
+++ functions/getFranchisesList.php
@@ -1,1 +1,14 @@
 <?php
+session_start();
+require "../conf.inc.php";
+require "../functions.php";
+header("Content-Type: application/json");
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("SELECT idUser, lastname, firstname, DATE_FORMAT(createDate, '%d/%m/%Y') as createDate, price FROM USER, TRANSACTION, SITEROLE 
+                                            WHERE user = idUser AND userRole = idRole AND roleName = 'Franchisé' 
+                                              AND transactionDate = createDate");
+$queryPrepared->execute();
+$result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($result);
