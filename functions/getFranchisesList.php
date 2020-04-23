739aa51a190009dd32e53f5fce5a29d3446cd787
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-23 14:02:51 +0200

    Display json franchises

--- functions/getFranchisesList.php
+++ functions/getFranchisesList.php
@@ -5,9 +5,9 @@ require "../functions.php";
 header("Content-Type: application/json");
 
 $pdo = connectDB();
-$queryPrepared = $pdo->prepare("SELECT idUser, lastname, firstname, DATE_FORMAT(createDate, '%d/%m/%Y') as createDate, price FROM USER, TRANSACTION, SITEROLE 
+$queryPrepared = $pdo->prepare("SELECT idUser, lastname, firstname, DATE_FORMAT(createDate, '%d/%m/%Y') as createDate, SUM(price) as price FROM USER, TRANSACTION, SITEROLE 
                                             WHERE user = idUser AND userRole = idRole AND roleName = 'Franchisé' 
-                                              AND transactionDate = createDate");
+                                              AND DATE_FORMAT(transactionDate, '%d/%m/%Y') = DATE_FORMAT(createDate, '%d/%m/%Y') GROUP BY idUser");
 $queryPrepared->execute();
 $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 
