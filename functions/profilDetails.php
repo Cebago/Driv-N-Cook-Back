f1ee2185de7248f21822d253bb9a0fe22b6d64f3
Baptiste DEMARCHE <batmine3@gmail.com>
2020-04-27 16:21:37 +0200

    Select city

--- functions/profilDetails.php
+++ functions/profilDetails.php
@@ -8,7 +8,7 @@ $email = $_SESSION["email"];
 $pdo = connectDB();
 $queryPrepared = $pdo->prepare("SELECT idUser, lastname, firstname, emailAddress, phoneNumber, roleName,
                                             DATE_FORMAT(createDate, '%d/%m/%Y') as createDate, address, postalCode, 
-                                            licenseNumber, fidelityCard
+                                            city, licenseNumber, fidelityCard
                                             FROM USER, SITEROLE WHERE idRole = USER.userRole AND emailAddress = :email");
 $queryPrepared->execute([":email" => $email]);
 $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
