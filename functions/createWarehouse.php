46fa2dad4592c62f130ef9e2dea266d01fe35733
Batmine3 <batmine3@gmail.com>
2020-05-03 23:21:36 +0200

    beginning of create warehouse

--- functions/createWarehouse.php
+++ functions/createWarehouse.php
@@ -1,1 +1,15 @@
 <?php
+require '../conf.inc.php';
+require '../functions.php';
+
+if ((count($_POST) == 4)
+    && isset($_POST['name'])
+    && isset($_POST['city'])
+    && isset($_POST['address'])
+    && isset($_POST['postalCode'])) {
+
+    $listOfErrors = "";
+    $error = false;
+
+
+}
