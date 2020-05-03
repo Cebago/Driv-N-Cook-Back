b7a260436819213bd329b733be269ced942664de
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-03 18:22:25 +0200

    code of the function

--- functions/getListWarehouses.php
+++ functions/getListWarehouses.php
@@ -1,1 +1,22 @@
 <?php
+require '../conf.inc.php';
+require '../functions.php';
+header("Content-Type: application/json");
+
+$pdo = connectDB();
+$queryPrepared = $pdo->prepare("SELECT idWarehouse, warehouseName, warehouseCity, warehouseAddress, warehousePostalCode FROM WAREHOUSES WHERE warehouseType = 'Entrepôt'");
+$queryPrepared->execute();
+$result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+
+for ($i = 0; $i < count($result); $i++) {
+    $queryPrepared = $pdo->prepare("SELECT COUNT(truck) as count FROM TRUCKWAREHOUSE WHERE warehouse = :warehouse");
+    $queryPrepared->execute([
+        ":warehouse" => $result[$i]['idWarehouse']
+    ]);
+    $count = $queryPrepared->fetch(PDO::FETCH_ASSOC);
+    $count = $count['count'];
+    $result[$i]['truck'] = $count;
+}
+
+
+echo json_encode($result);
