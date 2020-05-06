e800eb544cf53458f03c2a4a4b125cf66a9ddba5
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-06 18:55:14 +0200

    display also ingredients in the warehouse

--- functions/getListWarehouses.php
+++ functions/getListWarehouses.php
@@ -9,13 +9,22 @@ $queryPrepared->execute();
 $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 
 for ($i = 0; $i < count($result); $i++) {
-    $queryPrepared = $pdo->prepare("SELECT COUNT(truck) as count FROM TRUCKWAREHOUSE WHERE warehouse = :warehouse");
+
+    $queryPrepared = $pdo->prepare("SELECT COUNT(truck) as truck FROM TRUCKWAREHOUSE WHERE warehouse = :warehouse");
     $queryPrepared->execute([
         ":warehouse" => $result[$i]['idWarehouse']
     ]);
     $count = $queryPrepared->fetch(PDO::FETCH_ASSOC);
-    $count = $count['count'];
+    $count = $count['truck'];
     $result[$i]['truck'] = $count;
+
+    $queryPrepared = $pdo->prepare("SELECT COUNT(ingredient) as ingredient FROM STORE, WAREHOUSES, INGREDIENTS WHERE idIngredient = ingredient AND idWarehouse = warehouse AND available = true AND idWarehouse = :warehouse");
+    $queryPrepared->execute([
+        ":warehouse" => $result[$i]['idWarehouse']
+    ]);
+    $ingredient = $queryPrepared->fetch(PDO::FETCH_ASSOC);
+    $ingredient = $ingredient['ingredient'];
+    $result[$i]['ingredients'] = $ingredient;
 }
 
 
