61cd2ce965242887847309946b756d5b5b1d9f30
Baptiste DEMARCHE <batmine3@gmail.com>
2020-05-11 12:21:43 +0200

    update table of hours

--- functions/getOpenDays.php
+++ functions/getOpenDays.php
@@ -6,10 +6,20 @@ require "../functions.php";
 $truck = $_POST["truck"];
 
 $pdo = connectDB();
-$queryPrepared = $pdo->prepare("SELECT openDay, DATE_FORMAT(startHour,'%H:%i') as startHour, DATE_FORMAT(endHour,'%H:%i') as endHour  FROM OPENDAYS, TRUCK WHERE truck = idTruck AND truck = :truck");
+$queryPrepared = $pdo->prepare("SELECT openDay FROM pa2a2drivncook.OPENDAYS WHERE truck = :truck GROUP BY openDay ORDER BY DAYOFWEEK(openDay)");
 $queryPrepared->execute([
     ":truck" => $truck
 ]);
-$result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($result);
-echo $json;
+$day = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+$tmp = [];
+for ($i = 0; $i < count($day); $i++) {
+    $queryPrepared = $pdo->prepare("SELECT openDay, DATE_FORMAT(startHour,'%H:%i') as startHour, DATE_FORMAT(endHour,'%H:%i') as endHour FROM OPENDAYS WHERE truck = :truck AND openDay = :day ORDER BY hour(startHour)");
+    $queryPrepared->execute([
+        ":day" => $day[$i]["openDay"] ,
+        ":truck" => $truck
+    ]);
+    $open = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+    $tmp = array_merge($tmp, $open);
+}
+
+echo json_encode($tmp);
