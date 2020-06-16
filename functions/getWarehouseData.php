1463b4d4a85492378a5d515e66538f8137278524
Batmine3 <batmine3@gmail.com>
2020-06-16 21:26:47 +0200

    display availabiliy on warhouse

--- functions/getWarehouseData.php
+++ functions/getWarehouseData.php
@@ -15,7 +15,7 @@ $queryPrepared->execute([":warehouse" => $warehouse]);
 $result = $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
 for ($i = 0; $i < count($result); $i++) {
     $type = $result[$i]['label'];
-    $queryPrepared = $pdo->prepare("SELECT ingredientName FROM INGREDIENTS, STORE WHERE ingredientCategory = :type AND idIngredient = ingredient AND warehouse = :id");
+    $queryPrepared = $pdo->prepare("SELECT ingredientName, available FROM INGREDIENTS, STORE WHERE ingredientCategory = :type AND idIngredient = ingredient AND warehouse = :id");
     $queryPrepared->execute([
         ":type" => $type,
         ":id" => $warehouse
--- scripts/warehouse.js
+++ scripts/warehouse.js
@@ -14,40 +14,51 @@ function warehouseDisplay() {
         if(request.readyState === 4) {
             if(request.status === 200) {
                 let myJson = JSON.parse(request.responseText);
-                let chart = new CanvasJS.Chart("warehousePourcentage", {
-                    theme: "light2",
-                    animationEnabled: true,
-                    title: {
-                        text: "Pourcentage d'ingrédients différents - " + option
-                    },
-                    data: [{
-                        type: "pie",
-                        indexLabel: "{y}",
-                        yValueFormatString: "#,##0.00\"%\"",
-                        indexLabelPlacement: "inside",
-                        indexLabelFontColor: "#36454F",
-                        indexLabelFontSize: 18,
-                        indexLabelFontWeight: "bolder",
-                        showInLegend: true,
-                        legendText: "{label}",
-                        dataPoints: myJson
-                    }]
-                });
-                chart.render();
-                const ulP = document.createElement("ul");
-                for (let i = 0; i < myJson.length; i++) {
-                    const liP = document.createElement("li");
-                    liP.innerText = myJson[i]["label"];
-                    const ulC = document.createElement("ul");
-                    for (let j = 0; j < myJson[i]["ingredientList"].length; j++) {
-                        const liC = document.createElement("li");
-                        liC.innerText = myJson[i]["ingredientList"][j]["ingredientName"]
-                        ulC.appendChild(liC);
+                console.log(myJson);
+                if (myJson.length === 0) {
+                    const container = document.getElementById("warehousePourcentage");
+                    container.innerText = "Entrepôt vide"
+                } else {
+                    let chart = new CanvasJS.Chart("warehousePourcentage", {
+                        theme: "light2",
+                        animationEnabled: true,
+                        title: {
+                            text: "Pourcentage d'ingrédients différents - " + option
+                        },
+                        data: [{
+                            type: "pie",
+                            indexLabel: "{y}",
+                            yValueFormatString: "#,##0.00\"%\"",
+                            indexLabelPlacement: "inside",
+                            indexLabelFontColor: "#36454F",
+                            indexLabelFontSize: 18,
+                            indexLabelFontWeight: "bolder",
+                            showInLegend: true,
+                            legendText: "{label}",
+                            dataPoints: myJson
+                        }]
+                    });
+                    chart.render();
+                    const ulP = document.createElement("ul");
+                    for (let i = 0; i < myJson.length; i++) {
+                        const liP = document.createElement("li");
+                        liP.innerText = myJson[i]["label"];
+                        const ulC = document.createElement("ul");
+                        for (let j = 0; j < myJson[i]["ingredientList"].length; j++) {
+                            const liC = document.createElement("li");
+                            liC.innerText = myJson[i]["ingredientList"][j]["ingredientName"]
+                            if (Number(myJson[i]["ingredientList"][j]["available"]) === 1) {
+                                liC.innerText += " - Disponible"
+                            } else {
+                                liC.innerText += " - Indisponible"
+                            }
+                            ulC.appendChild(liC);
+                        }
+                        liP.appendChild(ulC);
+                        ulP.appendChild(liP);
                     }
-                    liP.appendChild(ulC);
-                    ulP.appendChild(liP);
+                    list.appendChild(ulP);
                 }
-                list.appendChild(ulP);
             }
         }
     };
