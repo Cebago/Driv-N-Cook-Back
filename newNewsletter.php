e8f63571492b0ab6111f19ea4e2e81f18f160e8a
Baptiste DEMARCHE <batmine3@gmail.com>
2020-06-24 21:22:50 +0200

    change display

--- newNewsletter.php
+++ newNewsletter.php
@@ -7,10 +7,26 @@ if (isAdmin() && isActivated() && isConnected()) {
     include 'header.php';
     ?>
     </head>
-
     <body>
     <?php include 'navbar.php'; ?>
-    New one
+    <div class="col-md-5 mx-auto mt-3 mb-3 card pt-3 pb-3 alert-secondary">
+        <div>
+            <p>test</p>
+        </div>
+        <div>
+            <p>test</p>
+        </div>
+        <div>
+            <p>test</p>
+        </div>
+        <div>
+            <p>test</p>
+        </div>
+    </div>
+    <div class="col-md-5 mx-auto mt-3 mb-3">
+        <button class="btn btn-primary"><i class="fas fa-plus"></i>&nbsp;Ajouter une zone de texte</button>
+        <button class="btn btn-secondary"><i class="far fa-images"></i>&nbsp;Ajouter une image</button>
+    </div>
     <?php include 'footer.php'; ?>
     </body>
     <?php
