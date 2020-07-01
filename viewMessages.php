61d587417d9eca0b086c74c2d2893260f247e38e
Pranksous <hugo91150@hotmail.fr>
2020-07-01 21:50:32 +0200

    add request SQL

--- viewMessages.php
+++ viewMessages.php
@@ -7,21 +7,31 @@ include 'header.php';
 ?>
 </head>
 <body>
-<?php include "navbar.php"; ?>
+<?php include "navbar.php";
+var_dump($_SESSION);
+?>
 
 <div class="container">
     <table class="table table-striped table-bordered table-hover table-hover-cursor" style="table-layout: fixed" id="tblData">
         <thead class="thead-light">
         <tr>
-            <th scope="col" data-bind="tableSort: { arr: _data, propName: 'text()'}">Text</th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>ID</th>
+            <th>Expediteur</th>
+            <th>Sujet</th>
+            <th>Contenu</th>
+            <th>Date</th>
+
         </tr>
         </thead>
         <tbody>
+        <?php
+            $messages = getMessages($_SESSION["email"]);
+            var_dump($messages);
+
+        ?>
+
         <tr>
-            <td class="text-truncate">wedwewe ewfwefwe</td>
+            <td class="text-truncate">ID Message</td>
             <td class="text-truncate"> ewfwefwe</td>
             <td class="text-truncate">wedwewe ewfwefwe</td>
             <td class="text-truncate">wedwewe ewfwefwe</td>
