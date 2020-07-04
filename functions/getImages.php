404b5e23cea40964e2ebaafcdb46a1f4adfe2906
Batmine3 <batmine3@gmail.com>
2020-07-04 11:14:52 +0200

    fix addImage

---
+++ functions/getImages.php
@@ -0,0 +1,16 @@
+<?php
+$allFiles = glob("../../newsletterImages/*");
+$images = [];
+for ($i = 0; $i < count($allFiles); $i++) {
+    $imageName = $allFiles[$i];
+    $support = array('gif', 'jpg', 'jpeg', 'png');
+    $ext = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
+    if (in_array($ext, $support)) {
+        //echo '<a onclick="addImage(this)" href="#"><img class="ml-2 mr-2 mt-2 mb-2" width="200px" height="200px" src="' . $imageName . '" alt="' . $imageName . '" /></a>';
+        $images[] = $imageName;
+    } else {
+        continue;
+    }
+}
+
+echo json_encode($images);
