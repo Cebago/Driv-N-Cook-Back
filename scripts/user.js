ee806a53797fe9ce633c124469f39062025b61b2
Pranksous <hugo91150@hotmail.fr>
2020-07-06 18:35:21 +0200

    finish addSearch

--- scripts/user.js
+++ scripts/user.js
@@ -2,3 +2,7 @@ function changeForm(user) {
     const form = document.getElementById("userForm");
     form.setAttribute("action", "functions/changeUserRole.php?id=" + user);
 }
+
+$(document).ready( function () {
+    $('#userTab').DataTable();
+} );
